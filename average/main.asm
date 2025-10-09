2507d0d99a4955893947831e276393e39a2d47fc
maximgf <maxim-a90@mail.ru>
2025-10-09 12:30:40 +0800

    data

--- average/main.asm
+++ average/main.asm
@@ -1,27 +1,30 @@
 section .data
-    array_x     dq  5, 3, 2, 6, 1, 7, 5
-    array_y     dq  0, 10, 1, 9, 2, 8, 5
-    ELEMENT_COUNT equ 7
-    ELEMENT_SIZE  equ 8
+    filename    db "data.txt", 0
     
-    msg_sum_diff db "Сумма разниц: ", 0
     msg_avg_quot db "Среднее (целое): ", 0
     msg_avg_rem  db "Остаток: ", 0
-    newline      db 0xA
-    
-    digit_space  db "           "
-    digits_len   equ $ - digit_space
+        
+    ELEMENT_COUNT equ 7
+    ELEMENT_SIZE  equ 8
+    MAX_FILE_SIZE equ 256
     
 section .bss
+    file_descriptor resq 1
+    file_buffer     resb MAX_FILE_SIZE
+    
+    array_x_mem     resq ELEMENT_COUNT
+    array_y_mem     resq ELEMENT_COUNT
+    
     total_difference resq 1
     average_quotient resq 1
     average_remainder resq 1
-    buffer resb 20
     
+    buffer          resb 20
+
 section .text
     global _start
 
-%macro PRINT_STRING 1
+%macro WRITE_STRING 1
     push    r8
     push    rcx
     push    rdx
@@ -51,7 +54,7 @@ section .text
     pop     r8
 %endmacro
 
-%macro PRINT_NUMBER 1
+%macro WRITE_NUMBER 1
     push    rdi
     push    rsi
     push    rdx
@@ -103,10 +106,49 @@ section .text
     pop     rdi
 %endmacro
 
+%macro EXIT 1
+    mov     rax, 60
+    mov     rdi, %1
+    syscall
+%endmacro
+
+%macro OPEN_FILE 2
+    mov     rax, 2
+    mov     rdi, %1
+    mov     rsi, %2
+    xor     rdx, rdx
+    syscall
+%endmacro
+
+%macro READ_FILE 3
+    mov     rax, 0
+    mov     rdi, %1
+    mov     rsi, %2
+    mov     rdx, %3
+    syscall
+%endmacro
+
+%macro CLOSE_FILE 1
+    mov     rax, 3
+    mov     rdi, %1
+    syscall
+%endmacro
+
 _start:
+    OPEN_FILE filename, 0
+    mov     [file_descriptor], rax
+    
+    mov     rdi, rax
+    READ_FILE rdi, file_buffer, MAX_FILE_SIZE
+    
+    mov     rsi, file_buffer
+    mov     rdi, array_x_mem
+    mov     r8, array_y_mem
+    call    parse_data
+    
     mov     rcx, ELEMENT_COUNT
-    mov     rsi, array_x
-    mov     rdi, array_y
+    mov     rsi, array_x_mem
+    mov     rdi, array_y_mem
     xor     r12, r12
     
 loop_start:
@@ -134,14 +176,124 @@ loop_start:
     mov     [average_quotient], rax
     mov     [average_remainder], rdx
 
-    PRINT_STRING msg_avg_quot
+    WRITE_STRING msg_avg_quot
+    WRITE_NUMBER [average_quotient]
     
-    PRINT_NUMBER [average_quotient]
-    
-    PRINT_STRING msg_avg_rem
-    
-    PRINT_NUMBER [average_remainder]
+    WRITE_STRING msg_avg_rem
+    WRITE_NUMBER [average_remainder]
 
-    mov     rax, 60
-    xor     rdi, rdi
-    syscall
+    CLOSE_FILE [file_descriptor]
+    EXIT 0
+    
+parse_data:
+    push    rbx
+    push    rcx
+    push    r9
+    push    r10
+    
+    mov     rbx, rdi
+    mov     r9, r8
+    mov     r10, 0
+    mov     rcx, ELEMENT_COUNT * 2
+    
+.loop_parse:
+    cmp     r10, rcx
+    je      .done_parse
+    
+    call    parse_number
+    
+    cmp     r10, ELEMENT_COUNT
+    jl      .save_x
+    jmp     .save_y
+    
+.save_x:
+    mov     [rbx], rax
+    add     rbx, 8
+    jmp     .next_element
+    
+.save_y:
+    mov     [r9], rax
+    add     r9, 8
+    
+.next_element:
+    inc     r10
+    jmp     .loop_parse
+    
+.done_parse:
+    mov     rax, 1
+    
+    pop     r10
+    pop     r9
+    pop     rcx
+    pop     rbx
+    ret
+
+parse_number:
+    push    rdi
+    push    rbx
+    push    rcx
+    push    rdx
+    
+    xor     rbx, rbx
+    mov     rdi, 0
+    mov     rdx, 0
+    
+.skip_whitespace:
+    mov     al, [rsi]
+    cmp     al, ' '
+    je      .move_pointer
+    cmp     al, 0xA
+    je      .move_pointer
+    cmp     al, 0xD
+    je      .move_pointer
+    cmp     al, ','
+    je      .move_pointer
+    jmp     .check_sign
+    
+.move_pointer:
+    inc     rsi
+    jmp     .skip_whitespace
+    
+.check_sign:
+    mov     al, [rsi]
+    cmp     al, '-'
+    jne     .parse_loop
+    mov     rdx, 1
+    inc     rsi
+    
+.parse_loop:
+    mov     al, [rsi]
+    cmp     al, '0'
+    jl      .end_parse
+    cmp     al, '9'
+    jg      .end_parse
+    
+    mov     rdi, 1
+    
+    sub     al, '0'
+    movzx   rcx, al
+    
+    mov     rax, rbx
+    mov     rbx, 10
+    mul     rbx
+    mov     rbx, rax
+    add     rbx, rcx
+    
+    inc     rsi
+    jmp     .parse_loop
+    
+.end_parse:
+    
+    cmp     rdx, 1
+    jne     .positive_result
+    neg     rbx
+    
+.positive_result:
+    mov     rax, rbx
+    
+.return_number:
+    pop     rdx
+    pop     rcx
+    pop     rbx
+    pop     rdi
+    ret
